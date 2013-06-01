ec1f06a58fa27f9177079e42c3ad1c686898a687
Fires10 <Fires10@gmail.com>
2013-06-01 07:48:17 -0300

    Update Taxes.py

--- Taxes.py
+++ Taxes.py
@@ -1,5 +1,23 @@
 class taxes:
-    Def __init__(married=0,dependents=0):
-
-
+    _married 
+    _dependents
+    _grosspay
+    _pretaxbenefits
+        
+    def __init__(married=0,dependents=0):
+         
+         self.married = married
+         self._dependents = dependents
+    def pretaxbenefits(ben):
+        
+        self._pretaxbenefits = ben
+        
+    def grosspay(pay):
+        
+        self._grosspay = pay
+        
+        
+    
+        
+    
        
