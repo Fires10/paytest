1b1152d17c811eb38a735e7f1877efe294cc9bf3
Fires10 <Fires10@gmail.com>
2013-06-16 11:48:21 -0300

    Update mymath.js

--- mymath.js
+++ mymath.js
@@ -10,7 +10,14 @@ function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomOpr()
+function getRandomOpr(a){
+  var b = "+";
+  if (a != null){
+   b = a;
+  }
+ 
+   return a; 
+}
 
 x = getRandomInt (x,y);
 y = getRandomInt(x,y);
