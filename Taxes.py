5e618f09757d9d6df4f984abde2a8367703dd51f
Fires10 <Fires10@gmail.com>
2013-12-31 16:08:46 -0500

    Update Taxes.py

--- Taxes.py
+++ Taxes.py
@@ -3,18 +3,29 @@ class taxes:
     _dependents
     _grosspay
     _pretaxbenefits
+    _taxablepay
         
     def __init__(married=0,dependents=0):
          
          self.married = married
          self._dependents = dependents
-    def pretaxbenefits(ben):
+    def pretaxbenefits(ben=0):
         
         self._pretaxbenefits = ben
         
     def grosspay(pay):
         
         self._grosspay = pay
+    
+    def taxablepay():
+        return self._grosspay - self._pretxbenefits
+        
+        
+    def medicare():
+        return self.taxablepay() * 0.0145
+        
+    def socialsecuritytax():
+        return self.taxablepay() * 0.062
         
         
     
